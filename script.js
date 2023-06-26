fe3e221ec3aa3d0f6edf02bddeaf153215e6a201
j-hazart <jessy.hazart@gmail.com>
2023-06-27 00:04:57 +0200

    add function to check win or draw

--- script.js
+++ script.js
@@ -13,5 +13,50 @@ for (let rowCase of rowCases) {
   rowCase.addEventListener("click", (e) => {
     rowCase.innerText = sign;
     handleSign();
+    checkDraw();
+    checkWin();
   });
 }
+
+const reset = () => {
+  for (let rowCase of rowCases) {
+    rowCase.innerText = "";
+  }
+};
+
+const winCases = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+const checkWin = () => {
+  for (let winCase of winCases) {
+    if (
+      rowCases[winCase[0]].innerText === rowCases[winCase[1]].innerText &&
+      rowCases[winCase[1]].innerText === rowCases[winCase[2]].innerText &&
+      rowCases[winCase[0]].innerText !== ""
+    ) {
+      alert(`${rowCases[winCase[0]].innerText} gagne`);
+      reset();
+    }
+  }
+};
+
+const checkDraw = () => {
+  let emptyCases = 0;
+  for (let rowCase of rowCases) {
+    if (rowCase.innerText === "") {
+      emptyCases++;
+    }
+  }
+  if (emptyCases === 0) {
+    alert("Match nul");
+    reset();
+  }
+};
